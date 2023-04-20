61bd7cc9a24a7c34f2b578be177cd2251e068be6
Harsh <harshhk608@gmail.com>
2023-04-20 20:57:43 +0530

    Add New topics

---
+++ src/Assignments/DupRemove.java
@@ -0,0 +1,27 @@
+package Assignments;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
+public class DupRemove {
+
+    public static List duplicateRemove(List<Integer> list) {
+        List<Integer> newlist = new ArrayList<Integer>();
+
+        for (Integer elements : list)
+            if (!newlist.contains(elements)) {//
+                newlist.add(elements);
+            }
+
+        return newlist;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5, 6,3, 7,8, 8,2,4, 9, 10));
+
+        System.out.println(duplicateRemove(list));    }
+
+}
